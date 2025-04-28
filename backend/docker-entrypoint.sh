7633ac1e666a5ade460da5da0bf4b0577fac3981
Yasin Walum <ywalum@gmail.com>
2025-04-29 02:47:12 +0300

    edited entry point

--- backend/docker-entrypoint.sh
+++ backend/docker-entrypoint.sh
@@ -3,10 +3,15 @@
 # Wait for the database to be ready
 if [ -n "$DATABASE_URL" ]; then
     echo "Waiting for database..."
-    until nc -z -v -w30 $(echo $DATABASE_URL | awk -F[/:@] '{print $5}') $(echo $DATABASE_URL | awk -F[/:@] '{print $6}')
-    do
-      echo "Waiting for database connection..."
-      sleep 1
+
+    host=$(echo "$DATABASE_URL" | sed -E 's|.*://[^@]+@([^:/]+):([0-9]+)/.*|\1|')
+    port=$(echo "$DATABASE_URL" | sed -E 's|.*://[^@]+@([^:/]+):([0-9]+)/.*|\2|')
+
+    echo "Connecting to host: $host, port: $port"
+
+    until nc -z -v -w30 "$host" "$port"; do
+        echo "Waiting for database connection..."
+        sleep 1
     done
 fi
 
